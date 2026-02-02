0d32ea229de3ce74ee445837948d27b998f7fd04
ajaydevas1 <162972970+ajaydevas1@users.noreply.github.com>
2026-02-02 12:51:47 +0530

    Update dataset paths in encode.py

--- src/preprocessing/encode.py
+++ src/preprocessing/encode.py
@@ -2,8 +2,8 @@ import pandas as pd
 from schema import NSL_KDD_COLUMNS
 
 # Paths
-TRAIN_PATH = "data/raw/KDDTrain+.txt"
-TEST_PATH  = "data/raw/KDDTest+.txt"
+TRAIN_PATH = "KDD_Train_Dataset"
+TEST_PATH  = "KDD_Test_Dataset"
 
 # Load raw data
 df_train = pd.read_csv(TRAIN_PATH, header=None)
